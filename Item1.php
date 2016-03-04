e448f84582c993a6185c2c566793a066f78176e0
Renata e Eduardo <Renata e Eduardo>
2016-03-04 04:03:43 -0300

    Atualizado testes

--- Item1.php
+++ Item1.php
@@ -1,56 +1,37 @@
 <?php
 print "Digite o sexo do cliente (M / F): ";
 $sexo = trim(fgets(STDIN));
+
 print "Quantidade de cervejas consumidas: ";
 $cerva = trim(fgets(STDIN));
+
 print "Quantidade de refrigerantes consumidos: ";
 $refri = trim(fgets(STDIN));
-print "Quantidade de espetos consumidos: ";
+
+print "Quantidade de espetinhos consumidos: ";
 $esp = trim(fgets(STDIN));
 
 $cons = (($cerva * 2.5) + ($refri * 2) + ($esp * 4));
 
-if (($sexo == "m" || $sexo == "M" ) && ($cons <=15)) {
-	$sub = $cons + 10 + 3;
-	$t =  $sub * 0.1 + $sub;
-	print "\nValor da conta:";
-	print "\nR$ 10 (Entrada)";
-	print "\nR$ $cons (Consumação)";
-	print "\nR$ 03 (Couvert)";
-	print "\nR$ $sub (Subtotal sem 10%)";
-	print "\n--------------------------------";
-	print "\nR$ $t (TOTAL)";
-	
-} else if (($sexo == "m" || $sexo == "M") && ($cons > 15)) {
-	$sub = $cons + 10;
-	$t =  $sub * 0.1 + $sub;
-	print "\nValor da conta:";
-	print "\nR$ 10 (Entrada)";
-	print "\nR$ $cons (Consumação)";
-	print "\nR$ 00 (Couvert)";
-	print "\nR$ $sub (Subtotal sem 10%)";
-	print "\n--------------------------------";
-	print "\nR$ $t (TOTAL)";
+if ($sexo == "m" || $sexo == "M" ) {
+	$ent = 10;	
 
-}else if (($sexo == "f" || $sexo == "F") && ($cons <= 15)) {
-	$sub = $cons + 8 + 3;
-	$t = $sub * 0.1 + $sub;
-	print "\nValor da conta:";
-	print "\nR$ 08 (Entrada)";
-	print "\nR$ $cons (Consumação)";
-	print "\nR$ 03 (Couvert)";
-	print "\nR$ $sub (Subtotal sem 10%)";
-	print "\n--------------------------------";
-	print "\nR$ $t (TOTAL)";
-	
-} else if (($sexo == "f" || $sexo == "F") && ($cons > 15)){
-	$sub = $cons + 8;
-	$t = $sub * 0.1 + $sub;
-	print "\nValor da conta:";
-	print "\nR$ 08 (Entrada)";
-	print "\nR$ $cons (Consumação)";
-	print "\nR$ 00 (Couvert)";
-	print "\nR$ $sub (Subtotal sem 10%)";
-	print "\n--------------------------------";
-	print "\nR$ $t (TOTAL)";
-}	
+} else
+	$ent = 8;
+
+if ($cons <= 15) {
+	$couv = 3;
+
+} else 
+	$couv = 0;
+
+$sub = $cons + $ent + $couv;
+$t =  $sub * 0.1 + $sub;
+
+print "\nValor da conta:";
+print "\nR$ $ent (Entrada)";
+print "\nR$ $cons (Consumação)";
+print "\nR$ $couv (Couvert)";
+print "\nR$ $sub (Subtotal sem 10%)";
+print "\n------------------------------";
+print "\nR$ $t (TOTAL)";
--- Item2.php
+++ Item2.php
@@ -1,13 +1,60 @@
 <?php
 print "Total de rendimentos bancários: ";
 $rendbanco = trim(fgets(STDIN));
+
 print "Total de rendimentos de salários ou serviços: ";
 $rendsalario = trim(fgets(STDIN));
+
 print "Total de outros rendimentos: ";
 $rendoutros = trim(fgets(STDIN));
+
 print "Serviços médicos pagos: ";
 $servmed = trim(fgets(STDIN));
+
 print "Serviços educacionais pagos: ";
 $serveduc = trim(fgets(STDIN));
 
-If ($rendbanco > 24000
+$ibanco = $rendbanco * 0.2;
+
+if ($rendsalario <= 8000) {
+	$isal = 0;
+	
+} else if ($rendsalario <= 24000) {
+	$isal = $rendsalario * 0.15;
+	
+} else if ($rendsalario > 24000) {
+	$isal = $rendsalario * 0.2;
+}
+
+$ioutros = $rendoutros * 0.1;
+$servtotal = $servmed + $serveduc;
+$itotal = $ibanco + $isal + $ioutros;
+$maxabat = $itotal * 0.3;
+
+if ($servtotal > $maxabat ) {
+	$abat = $maxabat;
+
+} else 
+	$abat = $servtotal;	
+	$imposto = $itotal - $abat;
+
+print "\nTotal de Impostos:";
+print "\nR$ $ibanco (sobre rendimentos bancários)";
+print "\nR$ $isal (sobre salários serviços)";
+print "\nR$ $ioutros (sobre outros rendimentos)";
+print "\nR$ $itotal (total)";
+print "\n-------------------------------------------------";
+print "\nMáximo a ser abatido:";
+print "\nR$ $maxabat";
+print "\n-------------------------------------------------";
+print "\nTotal de valores possíveis de abater:";
+print "\nR$ $servmed (serviçoes médicos)";
+print "\nR$ $serveduc (serviços educacionais)";
+print "\nR$ $servtotal (total)";
+print "\n--------------------------------------------------";
+print "\nImposto total:";
+print "\nR$ $itotal (imposto bruto)";
+print "\nR$ $abat (abatimnentos)";
+print "\nR$ $imposto (total)";
+
+		
